2384fa164f6256ef2d3b8c5155ed197f4bfa1e73
eldorado-22 <jumashevv992@gmail.com>
2023-04-23 08:20:47 +0600

    tsFirst

---
+++ index.js
@@ -0,0 +1,36 @@
+/////////// 15.04.23  ////////////
+// let num:number = 10
+// let str:string = "motion"
+// let user:any = 100
+var user = {
+    id: 1,
+    name: "Motion",
+    age: 20,
+    hobby: "music",
+    car: "mercedes"
+};
+console.table(user);
+//////////// 17.04.23 ////////////
+// <  > -> generics
+// type Types<T> = T
+// const num:Types<number> = 100
+// const str:Types<string> = "Hello World"
+// const isDone:Types<boolean> = true
+// console.log(str)
+// interface IUser {
+//     id: number
+//     name: string
+//     age: number
+//     hobby: string
+//     car: string
+// }
+//
+//
+// const user:Types<IUser> = {
+//     id: 1,
+//     name: "Eldar",
+//     age: 18,
+//     hobby: "Music",
+//     car: "Camry"
+// }
+// console.table(user)
---
+++ index.ts
@@ -0,0 +1,67 @@
+/////////// 15.04.23  ////////////
+// let num:number = 10
+// let str:string = "motion"
+// let user:any = 100
+
+// const num:number = 100
+// num = 10
+//
+// let num:number = 20
+// num = 39
+// console.log(num)
+
+
+// function myFunc (a:number ,b:number) {
+//    return a + b
+// }
+//
+// console.log(myFunc(10, 20)) // 30
+
+
+// interface IUser{
+//     id: number
+//     name: string
+//     age: number
+//     hobby: string
+//     car: string
+// }
+//
+// const user:IUser = {
+//     id: 1,
+//     name: "Motion",
+//     age: 20,
+//     hobby: "music",
+//     car: "mercedes"
+// }
+// console.table(user)
+
+
+//////////// 17.04.23 ////////////
+
+// <  > -> generics
+
+
+// type Types<T> = T
+// const num:Types<number> = 100
+// const str:Types<string> = "Hello World"
+// const isDone:Types<boolean> = true
+// console.log(str)
+
+// interface IUser {
+//     id: number
+//     name: string
+//     age: number
+//     hobby: string
+//     car: string
+// }
+//
+//
+// const user:Types<IUser> = {
+//     id: 1,
+//     name: "Eldar",
+//     age: 18,
+//     hobby: "Music",
+//     car: "Camry"
+// }
+// console.table(user)
+
